9cf1e68b4351ccba0fc42a73ef51922d6f99e98c
= <=>
2024-03-22 13:13:26 +0100

    Video Funktions

--- Frontend/VideoGame/videoGameFunctions.js
+++ Frontend/VideoGame/videoGameFunctions.js
@@ -1,22 +1,48 @@
 const domainOfAPI = "https://cgarort2508.ieszaidinvergeles.es/FakeNews/fakeNews/public/api/admin";
-const questionContainer = document.getElementById("contentQuestion");
-let currentQuestionAnswer; //Saves the answer of the current Question on the Website
-let currentQuestionExplenation; //Saves the explenation of the current Question on the Website
+const videoContainer = document.getElementById("video");
+const videoBtn1 = document.getElementById("opt1-btn");
+const videoBtn2 = document.getElementById("opt2-btn");
 
 window.onload = () => {
-  setFirstVideo();
+    console.log("test")
+    setFirstVideo();
 };
 
+//Set First Video
 async function setFirstVideo(){
-    const data = await fetchVideoFromApi();
+    //fetch data
+    const data = await fetchVideoFromAPI(4);
+    console.log(data)
+    videoContainer.src = data.videoUrl;
+    videoBtn1.innerText = data.buttonOption1;
+    videoBtn1.value = data.videoUrlOption1;
+    videoBtn2.innerText = data.buttonOption2;
+    videoBtn2.value = data.videoUrlOption2;
+  }
+
+//Fetches the Vieo from API
+async function fetchVideoFromAPI(id){
+    const response = await fetch(domainOfAPI + "/videoinfo/"+id);
+    const data = await response.json();
+    return data
 }
 
-async function fetchVideoFromApi(){
-    /* const response = await fetch(domainOfAPI + "/");
-    const data = await response.json();
-    return data; */
-    const data = {
-        
+//Sets the Video Changes for when a new Video is Selected
+async function changeVideo(newVideoId){
+    const data = await fetchVideoFromAPI(newVideoId);
+    if(data.resultText != ""){
+        document.getElementById("resultText").innerText = data.resultText;
+        document.getElementById("result").style.display = "block";
+        videoBtn1.style.display = "none";
+        videoBtn2.style.display = "none";
     }
-    return data
+    videoContainer.src = data.videoUrl;
+    videoBtn1.innerText = data.buttonOption1;
+    videoBtn1.value = data.videoUrlOption1;
+    videoBtn2.innerText = data.buttonOption2;
+    videoBtn2.value = data.videoUrlOption2; 
+}
+
+async function onClick(button){
+    changeVideo(button.value);
 }
