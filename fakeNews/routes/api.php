feb84aa78db00b1fedfb1a72d4a991ae6f4c8e34
Samuel Sánchez Heredia <113686086+SamuelSanchezHeredia@users.noreply.github.com>
2024-03-22 11:32:49 +0100

    Documentation

--- fakeNews/routes/api.php
+++ fakeNews/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\VideoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionImgController;
+use App\Http\Controllers\VideoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,12 @@ use App\Http\Controllers\QuestionImgController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('admin/questionimg', QuestionImgController::class)->except('edit','create');
-Route::get('quizimg', [QuestionImgController::class,'showQuiz']);
-Route::get('admin/videoinfo/{id}', [VideoController::class,'videoInfo']);
 
+// Define a resource route for QuestionImgController
+Route::resource('admin/questionimg', QuestionImgController::class);
 
+// Create a custom route to show quiz images
+Route::get('quizimg', [QuestionImgController::class, 'showQuiz']);
+
+// Define another resource route for VideoController
+Route::resource('admin/videoinfo', VideoController::class);
